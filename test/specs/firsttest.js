0803a1446bd0f4d1c2f22daf502761a7cbe951db
iDeception <evgeni.samoilenka@gmail.com>
2023-02-02 16:56:29 +0100

    Review repository creation test

--- test/specs/firsttest.js
+++ test/specs/firsttest.js
@@ -1,4 +1,5 @@
 import { expect } from "chai";
+import { faker } from '@faker-js/faker';
 const loggedinUser = 'iDeception';
 describe('Smoke Test', async () => {
     beforeEach(async () => {
@@ -7,12 +8,27 @@ describe('Smoke Test', async () => {
         await $('input[id="password"]').setValue('pkjqhjr3816');
         await $('input[value = "Sign in"]').click();
     })
-    it("Verify User", async () => {
-        await $('//summary[@aria-label="View profile and more"]').click();
-        const actualUsername = await $('//strong[@class="css-truncate-target"]').getText()
-        expect(loggedinUser).to.equal(actualUsername)
-    });
-    // it("Create a new repository", async () => {
 
-    // })
+    // it("Verify User", async () => {
+    //     await $('//summary[@aria-label="View profile and more"]').click();
+    //     const actualUsername = await $('//strong[@class="css-truncate-target"]').getText()
+    //     expect(loggedinUser).to.equal(actualUsername)
+    // });
+
+    it("Create a new repository", async () => {
+        await $('//div[@data-target="loading-context.details"]//a[@class="btn btn-sm btn-primary"]').click();
+        const randomWord1 = faker.word.adjective(7);
+        const randomWord2 = faker.word.noun(7);
+        await $('input[id="repository_name"]').setValue(randomWord1 + '-' + randomWord2);
+        await $('input[id="repository_description"]').setValue('This is test repository!');
+        await $('input[id="repository_auto_init"]').click();
+        await $('//button[contains(text(),"Create repository")]').click();
+        await browser.pause(5000)
+    })
+
+    afterEach(async () => {
+        await browser.closeWindow();
+    })
+
 })
+
